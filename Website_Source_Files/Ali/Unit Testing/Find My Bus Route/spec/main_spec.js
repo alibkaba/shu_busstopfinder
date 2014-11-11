8b477388bb473a5f0d9e414abf77e1a46b989b62
WATCHMEHEAL <alibkaba@gmail.com>
2014-11-10 19:03:12 -0500

    update

--- Website_Source_Files/Ali/Unit Testing/Find My Bus Route/spec/main_spec.js
+++ Website_Source_Files/Ali/Unit Testing/Find My Bus Route/spec/main_spec.js
@@ -1,27 +1,60 @@
-describe("Test Write_Coordinates", function(){
-    it("Variables", function (){
-        var action = "Write_Coordinates";
-        var Address = "Address";
-        var Latitude = "Latitude";
-        var Longitude = "Longitude";
-        var Write_Coordinates_Data = {Address: Address, Latitude: Latitude, Longitude: Longitude, action: action};
+describe("Coordinates", function(){
+
+    var action;
+    var Coordinates_ID;
+    var Address;
+    var Latitude;
+    var Longitude;
+    var Write_Coordinates_Data;
+    var Read_Coordinates;
+    var Coordinates_Data;
+    var Strip_Coordinates;
+    var Delete_Coordinate_Data;
+
+    beforeEach(function() {
+        Address = "Address";
+        Latitude = "Latitude";
+        Longitude = "Longitude";
+    });
+
+    afterEach(function() {
+        action = null;
+        Coordinates_ID = null;
+        Address = null;
+        Latitude = null;
+        Longitude = null;
+        Write_Coordinates_Data = null;
+        Read_Coordinates = null;
+        Coordinates_Data = null;
+        Strip_Coordinates = null;
+        Delete_Coordinate_Data = null;
+    });
+
+    it("Write_Coordinates", function (){
+        action = "Write_Coordinates";
+        //Write_Coordinates_Data = {Address: Address, Latitude: Latitude, Longitude: Longitude, action: action};
 
         expect(action).toContain("Write_Coordinates");
         expect(Address).toContain("Address");
         expect(Latitude).toContain("Latitude");
         expect(Longitude).toContain("Longitude");
-        //expect(Write_Coordinates_Data).toHaveBeenCalled();
+        //expect(Write_Coordinates_Data).toHaveBeenCalledWith({ Address: 'Address', Latitude: 'Latitude', Longitude: 'Longitude', action: 'Write_Coordinates' });
     });
-});
 
+    it("Read_Coordinates", function (){
+        action = "Read_Coordinates";
+    });
 
-describe("Test Delete_Coordinates", function(){
-    it("Variable", function (){
-        var action = "Delete_Coordinates";
-        var Coordinates_ID = "1";
-        var Delete_Coordinate_Data = {Coordinates_ID: Coordinates_ID, action: action};
+    it("Strip_Coordinates", function (){
+        action = "Strip_Coordinates";
+    });
+
+    it("Delete_Coordinates", function (){
+        action = "Delete_Coordinates";
+        Coordinates_ID = "1";
 
         expect(action).toContain("Delete_Coordinates");
         expect(Coordinates_ID).toContain("1");
+        //expect(Delete_Coordinate_Data).toHaveBeenCalled();
     });
 });
